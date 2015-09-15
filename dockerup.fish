c4db164fa2245317db5b6cf57e977630bb3869a4
Tamer Mohammed Abdul-Radi <tamer@abdulradi.com>
2015-09-15 09:41:22 +0100

    Updated dockerup to use docker machine
    Assumes you have one virtual box machine.

--- dockerup.fish
+++ dockerup.fish
@@ -1,14 +1,7 @@
 function dockerup
-    boot2docker init
-
-    for line in (boot2docker up)
-      switch $line
-        case '*set -x*'
-            eval $line
-      end
-    end
-
-    set -xU DOCKER_HOST $DOCKER_HOST
-    set -xU DOCKER_CERT_PATH $DOCKER_CERT_PATH
-    set -xU DOCKER_TLS_VERIFY $DOCKER_TLS_VERIFY
+    set -l name (docker-machine ls --filter driver=virtualbox -q)
+    docker-machine start $name > /dev/null
+    eval (docker-machine env $name)
+    echo "$name" is up \n
+    docker-machine ls --filter state=Running
 end
