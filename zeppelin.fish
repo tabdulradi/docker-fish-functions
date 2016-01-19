25a56d17ea917a03d7a8dc5dc2385bf6cc4e384a
Tamer Mohammed Abdul-Radi <tamer@abdulradi.com>
2016-01-19 12:03:54 +0000

    Update zeppelin.fish

--- zeppelin.fish
+++ zeppelin.fish
@@ -1,4 +1,4 @@
 function zeppelin
-  docker run --name zeppelin -p 8080:8080 -p 8081:8081 dylanmei/zeppelin:latest
-  open http://(docker-machine ip default):8081/     
+  open http://(docker-machine ip default):8080/     
+  docker start zeppelin; or docker run --name zeppelin -p 8080:8080 -p 8081:8081 dylanmei/zeppelin:latest
 end
